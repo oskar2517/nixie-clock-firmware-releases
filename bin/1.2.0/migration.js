072a333fc0e0f34a82d76f8948ab2548f1dd026a
Oskar <12718920+oskar2517@users.noreply.github.com>
2026-08-31 16:34:59 +0200

    add 1.2.0 release

---
+++ bin/1.2.0/migration.js
@@ -0,0 +1,7 @@
+if (config.wifiIdleTransmissionPower === undefined) {
+    config.wifiIdleTransmissionPower = 8; // 2 dBm
+}
+
+if (config.wifiConnectedTransmissionPower === undefined) {
+    config.wifiConnectedTransmissionPower = 78; // 19.5 dBm
+}
